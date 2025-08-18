0680904d90d48b9ebb12d70d13aa2da725cdcff2
fluidcat <Belin0@163.com>
2025-08-18 16:31:49 +0800

    feat: 增加语音识别、文本转语音支持

--- main.py
+++ main.py
@@ -2,20 +2,17 @@ import json
 import os
 import traceback
 import uuid
-from datetime import datetime
 
-import aiohttp
 from astrbot.api import AstrBotConfig
 from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.core import logger
 from astrbot.core.message.components import Plain, Record
-from astrbot.core.provider.entities import ProviderRequest
 from astrbot.core.utils.astrbot_path import get_astrbot_data_path
 from astrbot.core.utils.tencent_record_helper import tencent_silk_to_wav
-
-from .stt import STT
 from .oil_tool import OilTool
+from .stt import STT
+from .tts import TTS
 from .weather_tool import WeatherTool
 
 
@@ -34,7 +31,10 @@ class FluidCatToolPlugin(Star):
         self.weather_tool = WeatherTool(context, config, self)
         self.oil_tool = OilTool(context, config, self)
         self.stt = STT(context, config, self)
+        self.tts = TTS(context, config, self)
         self.config = config
+        self.enable_tts = config.get("enable_tts", False)
+        self.enable_stt = config.get("enable_stt", False)
         super().__init__(context)
 
     async def initialize(self):
@@ -47,9 +47,7 @@ class FluidCatToolPlugin(Star):
             location(string): 省市区县的拼音
         """
         resp, *_ = await self.weather_tool.get_weather_from_api(location)
-        return event.plain_result(
-            resp if isinstance(resp, str) else json.dumps(resp, ensure_ascii=False)
-        )
+        return resp if isinstance(resp, str) else json.dumps(resp, ensure_ascii=False)
 
     @filter.command("weather")
     async def get_weather_command(self, event: AstrMessageEvent, location: str):
@@ -66,7 +64,7 @@ class FluidCatToolPlugin(Star):
         yield event.plain_result(format_weather)
 
     @filter.llm_tool("current_oil_price_and_forecast")
-    async def get_weather_tool(self, event):
+    async def current_oil_price_and_forecast(self, event):
         """查询当前92号汽油、95号汽油、98号汽油、柴油的油价信息和下轮油价调整预测信息。
         """
         return json.dumps(await self.oil_tool.get_oil_json(), ensure_ascii=False)
@@ -85,6 +83,8 @@ class FluidCatToolPlugin(Star):
     async def voice_to_text(self, event: AstrMessageEvent):
         """收到语音消息时，将语音转为文本
         """
+        if not self.enable_stt:
+            return
         message_chain = event.get_messages()
 
         for idx, component in enumerate(message_chain):
@@ -96,6 +96,9 @@ class FluidCatToolPlugin(Star):
 
                     await tencent_silk_to_wav(path, output_path)
                     result = await self.stt.audio_acr(output_path, event.get_sender_id())
+                    logger.info(f"fluidcat voice_to_text result: {result}")
+                    if not result:
+                        return
                     message_chain[idx] = Plain(result)
                     event.message_str += result
                     event.message_obj.message_str += result
@@ -106,8 +109,18 @@ class FluidCatToolPlugin(Star):
                     logger.error(f"语音转文本失败: {e}")
     
     @filter.on_decorating_result()
-    async def text_to_vioce():
+    async def text_to_vioce(self, event: AstrMessageEvent):
         """使用语音回复语音消息"""
+        if not self.enable_tts:
+            return
+        if event.get_extra("is_record_req"):
+            result = event.get_result()
+            for idx, component in enumerate(result.chain):
+                if isinstance(component, Plain):
+                    file_path = await self.tts.textToVoice(component.text)
+                    logger.debug(f"fluidcat text_to_vioce result: {file_path}")
+                    result.chain.append(Record.fromFileSystem(file_path))
+
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
--- stt.py
+++ stt.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import aiohttp
 from astrbot.core import AstrBotConfig
 from astrbot.core.star import Context
+from astrbot.core import logger
 
 
 class STT:
@@ -14,11 +15,16 @@ class STT:
         self.config = config
         self.astrbotTool = astrbotTool
 
+        self.dify_stt_key = config.get("dify_stt_key", "")
+
     async def audio_acr(self, file_path: str, session_id: str) -> str:
+        if not self.dify_stt_key:
+            return None
+
         upload_file_id = await self.upload_file(file_path, session_id)
 
         url = 'https://api.dify.ai/v1/workflows/run'
-        headers = {"Authorization": f"Bearer app-WOBX22g8QogVRSfjOZnqAaLa", "Content-Type": "application/json"}
+        headers = {"Authorization": f"Bearer {self.dify_stt_key}", "Content-Type": "application/json"}
         payload = json.dumps({
             "inputs": {"audio": {"type": "audio", "transfer_method": "local_file", "upload_file_id": upload_file_id}},
             "user": session_id,
@@ -34,7 +40,7 @@ class STT:
         return ocr_ret.get('data', {}).get('outputs', {}).get('text', '')
 
     async def upload_file(self, file_path: str, session_id: str):
-        headers = {"Authorization": f"Bearer app-WOBX22g8QogVRSfjOZnqAaLa"}
+        headers = {"Authorization": f"Bearer {self.dify_stt_key}"}
 
         file_name = Path(file_path).name
         if file_path and (mime_types := mimetypes.guess_type(file_name)):
@@ -57,3 +63,25 @@ class STT:
                 resp_json = await resp.json()
 
         return resp_json.get("id", "")
+
+    def split_audio(file_path, max_segment_length_ms=60000):
+        """
+        分割音频文件
+        """
+        audio = AudioSegment.from_file(file_path)
+        audio_length_ms = len(audio)
+        if audio_length_ms <= max_segment_length_ms:
+            return audio_length_ms, [file_path]
+        segments = []
+        for start_ms in range(0, audio_length_ms, max_segment_length_ms):
+            end_ms = min(audio_length_ms, start_ms + max_segment_length_ms)
+            segment = audio[start_ms:end_ms]
+            segments.append(segment)
+        file_prefix = file_path[: file_path.rindex(".")]
+        format = file_path[file_path.rindex(".") + 1 :]
+        files = []
+        for i, segment in enumerate(segments):
+            path = f"{file_prefix}_{i+1}" + f".{format}"
+            segment.export(path, format=format)
+            files.append(path)
+        return audio_length_ms, files
